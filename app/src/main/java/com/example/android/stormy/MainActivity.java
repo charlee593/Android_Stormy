1eb0fbbcadd709f43019a5c52d041a59fedeccf7
charlee593 <charlee593@gmail.com>
2015-01-22 21:22:06 -0500

    Add alert dialog and it works

--- app/src/main/java/com/example/android/stormy/MainActivity.java
+++ app/src/main/java/com/example/android/stormy/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.stormy;
 
+import android.app.AlertDialog;
 import android.app.DownloadManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -8,6 +9,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
@@ -24,8 +26,8 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String apiKey = "8e4a6e62c5d7966c7f81be4e611fbe37";
-        String latitude = "37.8267";
-        String longitude = "-122.423";
+        double latitude = 37.8267;
+        double longitude = -122.423;
         String forecastURL = "https://api.forecast.io/forecast/" + apiKey + "/" + latitude
                 + "," + longitude;
 
@@ -33,14 +35,32 @@ public class MainActivity extends ActionBarActivity {
         Request request = new Request.Builder().url(forecastURL).build();
 
         Call call = client.newCall(request);
-        try {
-            Response response = call.execute();
-            if(response.isSuccessful()){
-                Log.v(TAG, response.body().string());
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+
             }
-        } catch (IOException e) {
-            Log.e(TAG, "Exception catch: ", e);
-        }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                try {
+                    if(response.isSuccessful()){
+                        Log.v(TAG, response.body().string());
+                    }
+                    else{
+                        alertUserAboutError();
+                    }
+                } catch (IOException e) {
+                    Log.e(TAG, "Exception catch: ", e);
+                }
+            }
+        });
+        Log.d(TAG, "Main UT code is running!");
+    }
+
+    private void alertUserAboutError() {
+        AlertDialogFragment dialog = new AlertDialogFragment();
+        dialog.show(getFragmentManager(), "error_dialog");
     }
 
 }
